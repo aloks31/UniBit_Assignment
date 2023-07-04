73622cfe8bd98eae98c1f4de792f9b35feba36b5
Alok Shekhawat <105915710+aloks31@users.noreply.github.com>
2023-07-05 04:49:22 +0530

    Add files via upload

--- UnibitGames/Main.java
+++ UnibitGames/Main.java
@@ -1,54 +1,93 @@
-public class Main {
 
+import java.util.*;
+
+public class Main
+{
     public static void main(String[] args) {
-
-        int[] arr = { 1, 3, 2, 2, -4, -6, -2, 8 };
+        
+        int[] arr = {1, 3, 2, 2, -4, 6, -2, 8};
         int target = 4;
 
-        int[][] firstCombination = findCombinations(arr, target);
-        System.out.println("First Combination for \"" + target + "\": " + Arrays.deepToString(firstCombination));
-
-        List<Integer> mergedList = mergeLists(firstCombination);
-        System.out.println("Merge Into a Single List: " + mergedList);
-
-        // int doubleTarget = target * 2;
-        // int[][] secondCombination = findCombinations(mergedArray, doubleTarget);
-        // System.out.println("Second Combination for \"" + doubleTarget + "\": " + Arrays.deepToString(secondCombination));
-
+        int[][] res = firstCombination(arr, target);
+		System.out.println("combination for first target  ->" + Arrays.deepToString(res));
+        
+        int[] ans = mergedArr(res);
+        System.out.println("merged array ->" + Arrays.toString(ans));
+        
+        System.out.println("combination for doubled target" + secondCombination(ans, target * 2));
+        
     }
+    
+    
+    
+    public static int[][] firstCombination(int[] nums, int target) {
+		Map<Integer, List<Integer>> map = new HashMap<>();
 
-    public static List<List<Integer>> findCombinations(List<Integer> list, int target) {
+		List<int[]> list = new ArrayList<>();
 
-        List<List<Integer>> combinations = new ArrayList<>();
-        Set<Integer> seen = new HashSet<>();
+		for (int i = 0; i < nums.length; i++) {
 
-        for (Integer num : list) {
-            int complement = target - num;
+			int number = target - nums[i];
 
-            if (seen.contains(complement)) {
-                combinations.add(Arrays.asList(num, complement));
+			if (map.containsKey(number)) {
+				List<Integer> indices = map.get(number);
+				for (int index : indices) {
+					list.add(new int[] { nums[index], nums[i] });
+				}
+			}
+			if (!map.containsKey(nums[i])) {
+				map.put(nums[i], new ArrayList<>());
+			}
+			map.get(nums[i]).add(i);
+		}
+
+		int[][] result = new int[list.size()][2];
+		for (int i = 0; i < list.size(); i++) {
+			result[i] = list.get(i);
+		}
+
+		return result;
+	}
+
+    public static int[] mergedArr(int[][] res) {
+       
+      List<Integer> list = new ArrayList<>();
+        for (int[] row : res) {
+            for (int col : row) {
+               list.add(col);
             }
-
-            seen.add(num);
         }
-
-        return combinations;
-    }
-
-    public static List<Integer> mergeLists(List<List<Integer>> lists) {
-        List<Integer> mergedList = new ArrayList<>();
-
-        for (List<Integer> list : lists) {
-            mergedList.addAll(list);
+      
+        int[] result = new int[list.size()];
+      
+       for (int i = 0 ; i < result.length ; i++){
+            result[i] = list.get(i);
         }
-
-        Collections.sort(mergedList);
-
-        return mergedList;
+		
+		Arrays.sort(result);
+		return result;
     }
+    
+    public static List<List<Integer>> secondCombination(int[] arr, int target) {
+		List<List<Integer>> res = new ArrayList<>();
 
+		int left = 0;
+		int right = arr.length - 1;
+		for (int a = 0; a < (arr.length) / (target / 2); a++) {
+		    
+			List<Integer> list = new ArrayList<>();
+			
+			list.add(arr[left++]);
+			list.add(arr[right--]);
+			list.add(arr[left++]);
+			list.add(arr[right--]);
+			
+			res.add(list);
 
+		}
 
+		return res;
 
-
-}    
+	}
+    
+}
